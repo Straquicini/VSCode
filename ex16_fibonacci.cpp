ccab5b2084f9ecd6d4f6bebdcd9add8db33db521
Straquicini <a14642@oficina.pt>
2024-11-25 16:59:21 +0000

    Add files via upload

---
+++ ex16_fibonacci.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+
+int fibonacci(int x){
+    int num, res;
+
+
+    if (num =1){
+        res < 1
+        if (num = 2) {
+            res < 1
+        }
+    }
+
+}
+
+int main(){
+    cout << fibonacci();
+
+    return 0;
+}
+
+
